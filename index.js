89825e82c9cc98f7a3a9cc23eeab322b35dc0de4
LiteralMetaphorical <blesic.andrej@gmail.com>
2019-06-21 20:32:36 +0200

    minor fixes and comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $("body").hide();
 
-var canvas = document.getElementById("canv");
-var height = 300;
-var width = 400;
+//initialize canvas
+let canvas = document.getElementById("canv");
+let height = 300;
+let width = 400;
 canvas.height = height;
 canvas.width = width;
 $("#canvasdiv").css("height", height + "px");
@@ -14,14 +15,15 @@ window.onload = function() {
 	$("body").show();
 }
 
-var x = 0;
-var y = 0;
-var key;
-var snakelength = 1;
-var snakearr = [[0, 0]];
-var foodarr = [50, 125];
+//initialize game variables
+let x = 0;
+let y = 0;
+let key; //last pressed key
+let snakelength = 1;
+let snakearr = [[0, 0]];
+let foodarr = [50, 125];
 
-
+//block snake from changing direction to the opposite direction, for example left -> right or up -> down
 function itemKeyDown(e) {
 	if ((key === 39 || key === undefined) && e.keyCode === 37) {
 		return;
@@ -36,7 +38,10 @@ function itemKeyDown(e) {
 	}
 }
 
+//start new game, reset variables
 function newGame() {
+	c.clearRect(0, 0, width, height);
+	$(".gameOver").hide();
 	x = 0;
 	y = 0;
 	key = 39;
@@ -46,16 +51,15 @@ function newGame() {
 	play();
 }
 
+//main game loop
 function play() {
-	$(".gameOver").hide();
-	c.clearRect(0, 0, width, height);
-	var interval = setInterval(function() {
+	let interval = setInterval(function() { //set main interval that updates canvas according to game state every 120 milliseconds
 		c.clearRect(0, 0, width, height);
 		c.fillStyle = "red";
 		c.fillRect(foodarr[0] + 1, foodarr[1] + 1, 23, 23);
 		c.fillStyle = "green";
 		snakearr.splice(0, snakearr.length - snakelength);
-		switch (key) {
+		switch (key) { //check last pressed key and change snake direction
 			case undefined:
 				x+=25;
 				break;
@@ -73,9 +77,9 @@ function play() {
 				break;
 		}
 		for (var i = 0; i < snakearr.length; i++) {
-			c.fillRect(snakearr[i][0] + 1, snakearr[i][1] + 1, 23, 23);
+			c.fillRect(snakearr[i][0] + 1, snakearr[i][1] + 1, 23, 23); //draw snake cells according to current snakearr state
 		}
-		if (c.getImageData(x + 2, y + 2, 1, 1).data[1] !== 0) {
+		if (c.getImageData(x + 2, y + 2, 1, 1).data[1] !== 0) { //check if snake has folded in on itself, if yes game over
 			snakelength++;
 			$(".gameOver").html("<h2>GAME OVER</h2>");
 			$(".gameOver").append("<h2>FINAL SCORE: " + (snakelength-2) + "</h2>");
@@ -84,7 +88,7 @@ function play() {
 			c.clearRect(0, 0, width, height);
 			return;
 		}
-		if (x > width - 25 || y > height - 25 || x < 0 || y < 0) {
+		if (x > width - 25 || y > height - 25 || x < 0 || y < 0) { //check if snake has gone out of bounds, if yes game over
 			snakelength++;
 			$(".gameOver").html("<h2>GAME OVER</h2>");
 			$(".gameOver").append("<h2>FINAL SCORE: " + (snakelength-2) + "</h2>");
@@ -94,18 +98,18 @@ function play() {
 			return;
 		}
 		snakearr.push([x, y]);
-		if (x === foodarr[0] && y === foodarr[1]) {
+		if (x === foodarr[0] && y === foodarr[1]) { //check if snake is eating food
 			snakelength++;
 			c.clearRect(foodarr[0] + 1, foodarr[1] + 1, 23, 23);
 			c.fillRect(foodarr[0] + 1, foodarr[1] + 1, 23, 23);
-			do {
+			do { //place new piece of food on random spot on canvas not occupied by snake itself
 				foodarr[0] = Math.floor(Math.random()*(width/25))*25;
 				foodarr[1] = Math.floor(Math.random()*(height/25))*25;
 			}
 			while ((c.getImageData(foodarr[0] + 1, foodarr[1] + 1, 1, 1).data[2]) !== 0 || (c.getImageData(foodarr[0] + 1, foodarr[1] + 1, 1, 1).data[1]) !== 0);
-			c.fillStyle = "red";
+			c.fillStyle = "red"; //set food fill color
 		}
-		c.fillStyle = "green";
+		c.fillStyle = "green"; //set snake fill color
 		for (var i = 0; i < snakearr.length; i++) {
 			c.fillRect(snakearr[i][0] + 1, snakearr[i][1] + 1, 23, 23);
 		}
